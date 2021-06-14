42ddcc7aa2024dc87ba27353606f48a2f8f3fe47
Pauline Boband <paulineboband@gmail.com>
2021-06-14 17:31:27 +0200

    début des actions studios

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,2 +1,29 @@
 class StudiosController < ApplicationController
+  def index
+    @studios = Studio.all
+  end
+
+  def show
+    @studio = Studio.find(params[:id])
+  end
+
+  def new
+    @studio = Studio.new
+  end
+
+  def create
+    @studio = Studio.new(studio_params)
+    @studio.user = current_user
+    if @studio.save
+      redirect_to studio_path(@studio)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def studio_params
+    params.require(:studio).permit(:title, :address, :description, :photos, :price, :capacity)
+  end
 end
