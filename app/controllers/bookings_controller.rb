dba5d6369e0509c05c9ad576d3edd97aa83a026f
Timothee Rouquette <timothee.rouquette@edhec.com>
2021-06-15 10:16:45 +0200

    booking controller not ready

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,29 @@
 class BookingsController < ApplicationController
+  def new
+    @studio = Studio.find(params[:studio_id])
+    @booking = Booking.new()
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @studio = List.find(params[:studio_id])
+    @booking.studio = @studio
+    @user = User.find(params[:booking][:user_id])
+    @booking.user = @user
+    if @booking.save
+      redirect_to studio_path(@studio)
+    else
+      redirect_to new_studio_booking_path(@studio)
+    end
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:studio, :user_id, :comment)
+  end
 end
