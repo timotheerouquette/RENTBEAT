be93b97a0a633b3788add93783b3e1a340f159d7
Pauline Boband <paulineboband@gmail.com>
2021-06-15 10:20:25 +0200

    actions creation for Studios controllers

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -21,6 +21,24 @@ class StudiosController < ApplicationController
     end
   end
 
+  def edit
+    @studio = Studio.find(params[:id])
+  end
+
+  def update
+    @studio = Studio.find(params[:id])
+    @studio.update(studio_params)
+
+    redirect_to studio_path(@studio)
+  end
+
+  def destroy
+    @studio = Studio.find(params[:id])
+    @studio.destroy
+
+    redirect_to studios_path
+  end
+
   private
 
   def studio_params
