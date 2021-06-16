2421ad99c97f41d0a2ca9ab89e68b898efebefbd
Pauline Boband <paulineboband@gmail.com>
2021-06-16 12:31:50 +0200

    css modif home page et index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    studios = Studio.all
+    sorted = studios.sort_by(&:created_at)
+    @top = sorted.reverse.first(3)
   end
 end
